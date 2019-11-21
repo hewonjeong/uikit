5b085e06b83ef8ebc5ad90a2a9c58c054abbfdd6
Hewon Jeong <hewon.jng@gmail.com>
2019-11-21 16:39:00 +0900

    add required props to Button

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -8,15 +8,29 @@ type ButtonProps = {
   onClick?: (e?: React.MouseEvent<HTMLButtonElement>) => void
   /** 버튼의 생김새를 설정합니다. */
   theme: 'primary' | 'secondary' | 'tertiary'
+  /** 버튼의 크기를 설정합니다 */
+  size: 'small' | 'medium' | 'big'
+  /** 버튼을 비활성화 시킵니다. */
+  disabled: boolean
 }
 
 /** `Button` 컴포넌트는 어떠한 작업을 트리거 할 때 사용합니다.  */
-const Button = ({ children, onClick }: ButtonProps) => {
+const Button = ({ children, theme, size, disabled, onClick }: ButtonProps) => {
   return (
-    <button css={style} onClick={onClick}>
+    <button
+      css={[style, themes[theme], sizes[size]]}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {children}
     </button>
   )
+}
+
+Button.defaultProps = {
+  theme: 'primary',
+  size: 'medium2',
+  disabled: false
 }
 
 const style = css`
@@ -40,38 +54,68 @@ const style = css`
   &:active {
     background: #12b886;
   }
+  &:disabled {
+    cursor: not-allowed;
+  }
 `
 
 const themes = {
   primary: css`
     background: #20c997;
     color: white;
-    &:hover {
+    &:hover:enabled {
       background: #38d9a9;
     }
-    &:active {
+    &:active:enabled {
       background: #12b886;
+    }
+    &:disabled {
+      background: #aed9cc;
     }
   `,
   secondary: css`
     background: #e9ecef;
     color: #343a40;
-    &:hover {
+    &:hover:enabled {
       background: #f1f3f5;
     }
-    &:active {
+    &:active:enabled {
       background: #dee2e6;
+    }
+    &:disabled {
+      color: #c6d3e1;
     }
   `,
   tertiary: css`
     background: none;
     color: #20c997;
-    &:hover {
+    &:hover:enabled {
       background: #e6fcf5;
     }
-    &:active {
+    &:active:enabled {
       background: #c3fae8;
     }
+    &:disabled {
+      color: #bcd9d0;
+    }
+  `
+}
+
+const sizes = {
+  small: css`
+    height: 1.75rem;
+    font-size: 0.75rem;
+    padding: 0 0.875rem;
+  `,
+  medium: css`
+    height: 2.5rem;
+    font-size: 1rem;
+    padding: 0 1rem;
+  `,
+  big: css`
+    height: 3rem;
+    font-size: 1.125rem;
+    padding: 0 1.5rem;
   `
 }
 
