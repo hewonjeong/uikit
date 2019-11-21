462d14ebc4715d87baa4ebdadddd4878122fd08b
Hewon Jeong <hewon.jng@gmail.com>
2019-11-21 14:49:40 +0900

    fix typo

--- src/Hello/Hello.js
+++ src/Hello/Hello.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Hello = ({ name, big }) => {
   if (big) {
-    return <h1>안녕하세요, ${name}</h1>
+    return <h1>안녕하세요, {name}</h1>
   }
-  return <p>안녕하세요. ${nmae}</p>
+  return <p>안녕하세요. {name}</p>
 }
 
 export default Hello
