1e7c0c9f2e8f5c22f1ef6c70d43aad86a1c906a3
Hewon Jeong <hewon.jng@gmail.com>
2019-11-21 17:19:44 +0900

    add config to load svg

--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1,1 +1,12 @@
 declare module '*.mdx' // 추후 ts 파일에서 mdx 확장자로 이루어진 파일을 불러오게 될 때 모듈이 없다는 에러를 방지하는 것 입니다.
+
+declare module '*.svg' {
+  import * as React from 'react'
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<
+    SVGSVGElement
+  >>
+
+  const src: string
+  export default src
+}
