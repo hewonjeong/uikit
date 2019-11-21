bbe93b10bcaf86d936cdce2b20233ef738fc6415
Hewon Jeong <hewon.jng@gmail.com>
2019-11-21 15:32:46 +0900

    add Hello mdx

--- src/Hello/Hello.stories.js
+++ src/Hello/Hello.stories.js
@@ -2,11 +2,18 @@ import React from 'react'
 import Hello from './Hello'
 import { withKnobs, text, boolean } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
+import mdx from './Hello.mdx'
 
 export default {
   title: 'components|basic/Hello', // 메뉴 리스트에서 차지할 이름!
   component: Hello,
-  decorators: [withKnobs] // 애드온 적용
+  decorators: [withKnobs], // 애드온 적용
+  parameters: {
+    componentSubtitle: '"안녕하세요"라고 보여주는 컴포넌트',
+    docs: {
+      page: mdx
+    }
+  }
 }
 
 export const hello = () => {
