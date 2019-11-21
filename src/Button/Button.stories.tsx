a09054ef33e566d46b089e72b0c37210194066e0
Hewon Jeong <hewon.jng@gmail.com>
2019-11-21 16:49:15 +0900

    add knobs of Button component

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,14 +1,33 @@
 /** @jsx jsx */
 import Button from './Button'
 import { jsx, css } from '@emotion/core'
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 
 export default {
   title: 'components|Button',
-  component: Button
+  component: Button,
+  decorators: [withKnobs]
 }
 
 export const button = () => {
-  return <Button>BUTTON</Button>
+  const label = text('children', 'BUTTON')
+  const size = select('size', ['small', 'medium', 'big'], 'medium')
+  const theme = select('theme', ['primary', 'secondary', 'tertiary'], 'primary')
+  const disabled = boolean('disabled', false)
+  const width = text('width', '')
+
+  return (
+    <Button
+      size={size}
+      theme={theme}
+      disabled={disabled}
+      width={width}
+      onClick={action('onClick')}
+    >
+      {label}
+    </Button>
+  )
 }
 
 button.story = {
